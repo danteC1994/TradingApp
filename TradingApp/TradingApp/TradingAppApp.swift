249e89fc17fb71c3d65c70b32904b223c4feba3d
dante canizo <dantecanizo@dantes-MacBook-Pro-2.local>
2024-03-07 08:52:21 -0300

    Project kick off

---
+++ TradingApp/TradingApp/TradingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingAppApp.swift
+//  TradingApp
+//
+//  Created by dante canizo on 07/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TradingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

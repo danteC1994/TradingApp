0396df9367205a925804267640c500e20d881bf6
dante canizo <dantecanizo@dantes-MacBook-Pro-2.local>
2024-03-11 16:59:09 -0300

    Avoid showing main view when running tests

--- TradingApp/App/TradingApp.swift
+++ TradingApp/App/TradingApp.swift
@@ -8,6 +8,23 @@
 import SwiftUI
 
 @main
+struct AppLauncher {
+    static func main() throws {
+        if NSClassFromString("XCTestCase") == nil {
+            TradingApp.main()
+        } else {
+            TestApp.main()
+        }
+    }
+}
+
+struct TestApp: App {
+
+    var body: some Scene {
+        WindowGroup { Text("Running Unit Tests") }
+    }
+}
+
 struct TradingApp: App {
     var body: some Scene {
         WindowGroup {
